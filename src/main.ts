f6e29c75cfa7dcf884b0753a32b308563053d1ab
strukovd <dmitryi97@yandex.ru>
2023-09-28 23:45:01 +0600

    init simple server

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import * as Hapi from '@hapi/hapi';
+import {Request, ResponseToolkit, Server} from 'hapi';
+
+class Main {
+	static async initServer() {
+		const server: Hapi.Server = Hapi.server({
+			host: `localhost`,
+			port: 5555
+		});
+
+		server.route({
+			method: `GET`,
+			path: `/`,
+			handler: (request: Request, h: ResponseToolkit, error: Error) => {
+				return {success: true};
+			}
+		});
+
+		await server.start();
+		console.log(`Server started at localhost:5555`);
+	}
+}
+
+Main.initServer();

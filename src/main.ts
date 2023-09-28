14d9b0972a61ac784b3977eaaf4ebd5aae34289b
strukovd <dmitryi97@yandex.ru>
2023-09-28 23:53:51 +0600

    added and fix eslint

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import * as Hapi from '@hapi/hapi';
-import {Request, ResponseToolkit, Server} from 'hapi';
+import { Request, ResponseToolkit } from 'hapi';
 
 class Main {
 	static async initServer() {
 		const server: Hapi.Server = Hapi.server({
 			host: `localhost`,
 			port: 5555
-		});
+	   	});
 
 		server.route({
 			method: `GET`,
 			path: `/`,
 			handler: (request: Request, h: ResponseToolkit, error: Error) => {
-				return {success: true};
+				return { success: true };
 			}
 		});
 
